adade2567b4e3a1e2ff456d62428908ba2247009
TonderaiKawere <tondeskawere@gmail.com>
2026-08-13 20:56:53 +0200

    Add PHP server interactive SVG mascot

--- app.js
+++ app.js
@@ -810,3 +810,4 @@ window.addEventListener("DOMContentLoaded", () => {
 const mascotSVGs = {};
 mascotSVGs.python = `<svg width='50' height='50' viewBox='0 0 100 100'></svg>`;
 mascotSVGs.c = `<svg width='50' height='50' viewBox='0 0 100 100'></svg>`;
+mascotSVGs.php = `<svg width='50' height='50' viewBox='0 0 100 100'></svg>`;
