007a059a6511aaeadd7f12060f77e880ed5a2cc6
TonderaiKawere <tondeskawere@gmail.com>
2026-08-14 20:54:44 +0200

    Add JavaScript external learning official references

--- data/resources.js
+++ data/resources.js
@@ -3,3 +3,7 @@ const resources = {
     { name: 'MDN Web Docs', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML', desc: 'The official and most detailed guide to HTML elements.' },
     { name: 'W3Schools CSS Tutorial', url: 'https://www.w3schools.com/css/', desc: 'Easy tutorial with sandbox code executors.' }
   ],
+  javascript: [
+    { name: 'Javascript.info', url: 'https://javascript.info/', desc: 'Modern tutorial outlining complex closures, scopes, and properties.' },
+    { name: 'MDN Javascript Guide', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript', desc: 'Official API documentation mapping browser APIs.' }
+  ],
