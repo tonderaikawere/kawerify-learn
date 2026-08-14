214bae3af0930df52bbc0a021d9c1f7d95831b25
TonderaiKawere <tondeskawere@gmail.com>
2026-08-14 20:54:38 +0200

    Add HTML CSS external learning references

--- data/resources.js
+++ data/resources.js
@@ -1,1 +1,5 @@
 const resources = {
+  html_css: [
+    { name: 'MDN Web Docs', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML', desc: 'The official and most detailed guide to HTML elements.' },
+    { name: 'W3Schools CSS Tutorial', url: 'https://www.w3schools.com/css/', desc: 'Easy tutorial with sandbox code executors.' }
+  ],
